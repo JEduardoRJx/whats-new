25ce8a90b46809fca2430dd2534f23e22ff8d895
Eduardo <jeduardorjx@gmail.com>
2019-10-01 19:48:19 -0600

    Add a NewsContainer component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
 import './App.css';
+import NewsContainer from '../NewsContainer/NewsContainer.js'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      localNews: local
     }
   }
 
   render () {
     return (
       <div className="app">
-        YOUR CODE GOES HERE!
+        <NewsContainer localNews={this.state.localNews} />
       </div>
     );
   }
