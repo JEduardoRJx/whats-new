e2efbd5dcdfb38e084837aa556085040a717f3ff
Eduardo <jeduardorjx@gmail.com>
2019-10-04 21:33:45 -0600

    Test changeArticle method in App

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import local from '../../data/local'
 
 describe('App', () => {
   let wrapper;
@@ -12,5 +13,29 @@ describe('App', () => {
   it('should create a snapshot of App', () => {
     expect(wrapper).toMatchSnapshot()
   });
+
+  it('should update state when changeArticle is called', () => {
+    //setup
+    const wrapper = shallow(<App />)
+    const mockEvent = {
+      target: {
+        className: 'localNews'
+      }
+    }
+    // const mockLocalNews = {
+    //   id: 1,
+    //   headline: 'headline',
+    //   img: 'img url',
+    //   description: 'description',
+    //   url: 'url'
+    // }
+    const expected = local
+    
+    //execution
+    wrapper.instance().changeArticle(mockEvent);
+
+    //expectation
+    expect(wrapper.state('news')).toEqual(expected)
+  })
 
 });
