abd5ddbd378efe1dbc37529131fd30c8413332f7
Eduardo <jeduardorjx@gmail.com>
2019-10-05 20:53:42 -0600

    Setup img tag for icons within buttons

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './Menu.css'
+import localIcon from './images/local.svg'
+import technologyIcon from './images/technology.svg'
+import entertainmentIcon from './images/entertainment.svg'
+import scienceIcon from './images/science.svg'
+import healthIcon from './images/health.svg'
 
 const Menu = ({ changeArticle }) => {
-  // console.log("hi", changeArticle)
   return (
     <nav>
       <h1>What's <span>New?</span></h1>
@@ -10,29 +14,44 @@ const Menu = ({ changeArticle }) => {
       className='localNews' 
       type='button' 
       onClick={event => changeArticle(event)} 
-      >IMG Local News</button>
+      >
+        <img className='icon' src={localIcon} alt='local icon' />
+        Local News
+        </button>
 
       <button 
       className='technologyNews' 
       type='button'
       onClick={event => changeArticle(event)} 
-      >IMG Technology</button>
+      >
+        <img className='icon' src={technologyIcon} alt='technology icon' /> 
+        Technology
+      </button>
 
       <button 
       className='entertainmentNews' 
       type='button' 
       onClick={event => changeArticle(event)} 
-      >IMG Entertainment</button>
+      >
+        <img className='icon' src={entertainmentIcon} alt='entertainment icon' />
+        Entertainment
+      </button>
 
       <button className='scienceNews' 
       type='button' 
       onClick={event => changeArticle(event)} 
-      >IMG Science</button>
+      >
+        <img className='icon' src={scienceIcon} alt='science icon' />
+        Science
+      </button>
 
       <button className='healthNews' 
       type='button' 
       onClick={event => changeArticle(event)} 
-      >IMG Health</button>
+      >
+        <img className='icon' src={healthIcon} alt='health icon' />
+        Health
+      </button>
     </nav>
   )
 }
