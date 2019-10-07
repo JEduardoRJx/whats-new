8cac5ef00ced72abdd9930239a3f88ec0d17e696
Eduardo <jeduardorjx@gmail.com>
2019-10-06 22:25:46 -0600

    Updates

---
+++ precache-manifest.fd87eabb99678457488cd0e83b465ec3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3b9b42741f846ef2a55efc2cf3b25d",
+    "url": "/JEduardoRJx/whats-new/index.html"
+  },
+  {
+    "revision": "3cb6f1fabbe26cc320c8",
+    "url": "/JEduardoRJx/whats-new/static/css/main.90dbe5c5.chunk.css"
+  },
+  {
+    "revision": "1778746e37ac1a6775f1",
+    "url": "/JEduardoRJx/whats-new/static/js/2.78bb1b38.chunk.js"
+  },
+  {
+    "revision": "3cb6f1fabbe26cc320c8",
+    "url": "/JEduardoRJx/whats-new/static/js/main.923b1445.chunk.js"
+  },
+  {
+    "revision": "e79af34fae18804733e7",
+    "url": "/JEduardoRJx/whats-new/static/js/runtime-main.b9d3dceb.js"
+  },
+  {
+    "revision": "4ab38e56522752637a94cb80fd0ac1aa",
+    "url": "/JEduardoRJx/whats-new/static/media/entertainment.4ab38e56.svg"
+  },
+  {
+    "revision": "6c5bdeff828128a5053540aa60bb31c8",
+    "url": "/JEduardoRJx/whats-new/static/media/health.6c5bdeff.svg"
+  },
+  {
+    "revision": "4479c65069b1d7a1938dba4e913c6b0e",
+    "url": "/JEduardoRJx/whats-new/static/media/local.4479c650.svg"
+  },
+  {
+    "revision": "272ce4a537139af2d29fe7ad2ed45dce",
+    "url": "/JEduardoRJx/whats-new/static/media/science.272ce4a5.svg"
+  },
+  {
+    "revision": "a24bba27f05300d6a0ad4978875f353b",
+    "url": "/JEduardoRJx/whats-new/static/media/technology.a24bba27.svg"
+  }
+]);
