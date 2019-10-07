328167774543cd987e4bafcb80c40a116f7dedb9
Eduardo <jeduardorjx@gmail.com>
2019-10-06 21:45:49 -0600

    Add an a tag for article links

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './NewsArticle.css';
 
 const NewsArticle = (props) => {
+  console.log(props.url)
   return (
     <article>
       <img src={props.img} alt='Article' />
       <h2>{props.headline}</h2>
       <p>{props.description}</p>
+      <a href={props.url} target='_blank'>READ MORE HERE --></a>
     </article>
   )
 }
